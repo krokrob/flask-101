124adef42cabdd0a3402f46a46667b63b3482746
kevin robert <kevin.j.robert@gmail.com>
2020-03-31 15:33:08 +0200

    add landing

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
 @app.route('/<name>')
 def home(name=None):
     name = name.capitalize()
