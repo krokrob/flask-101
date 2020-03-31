4d6be3429ea8bf1f0d50c9c600d47af6d6c4f013
kevin robert <kevin.j.robert@gmail.com>
2020-03-31 15:29:54 +0200

    test templates and variables

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/<name>')
+def home(name=None):
+    name = name.capitalize()
+    stack = ['Python', 'Flask', 'Heroku']
+    return render_template('home.html', name=name, stack=stack)
+
+@app.route('/new')
+def new():
+    return render_template('new.html.jinja')
+
+@app.route('/predict')
+def predict(input=None):
+    pass
